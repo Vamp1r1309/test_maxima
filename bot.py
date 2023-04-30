006d3a8241e3e1289057db0344a3c2fab108b1ae
Vamp1r1309 <64798961+Vamp1r1309@users.noreply.github.com>
2023-04-30 20:39:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,51 +1,24 @@
 import logging
 
-from aiogram import Bot, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import Dispatcher
-from aiogram.dispatcher.webhook import SendMessage
-from aiogram.utils.executor import start_webhook
+from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = 'BOT_TOKEN_HERE'
-
-# Настройка Webhook
-WEBHOOK_HOST = 'https://a120-95-25-161-16.ngrok-free.app' # -> Адрес сервера
-WEBHOOK_PATH = ''# -> Путь до api, где слушает бот
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}' # -> URL на который будут приниматься запросы
-
-# Настройка web-сервера
-WEBAPP_HOST = 'localhost' # -> хост нашего приложения, оставляем локальным
-WEBAPP_PORT = 8000 # -> port на котором работает наше приложение
+API_TOKEN = 'Ваш токен'
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token='6261025312:AAGVVr4uPV7YBZIQ33RqXGr87YtoDzfbBYc')
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
 
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-async def on_shutdown(dp):
-    logging.warning('Shutting down...')
-    await bot.delete_webhook()
-    logging.warning('Bye!')
 
 @dp.message_handler(commands=['start'])
-async def echo(msg: types.Message):
-    return SendMessage(msg.chat.id, msg.text)
+async def echo(message: types.Message):
+   await message.reply('Start')
+
 
 @dp.message_handler(commands=['help'])
-async def echo(msg: types.Message):
-    return SendMessage(msg.chat.id, 'Вы обратились к справке бота')
+async def echo(message: types.Message):
+   await message.reply('Вы обратились к справке бота')
 
-if __name__ == "__main__":
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
